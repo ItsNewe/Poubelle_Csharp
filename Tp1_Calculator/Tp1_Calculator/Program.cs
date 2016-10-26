3506a40e8c0dab72178c45b029d7db737e78f95c
Newe <newepone@derpymail.org>
2016-10-26 20:05:45 +0200

    add

---
+++ Tp1_Calculator/Tp1_Calculator/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp1_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Enter first value... ");
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter second value...");
+                int y = Convert.ToInt32(Console.ReadLine());
+
+                int sum = x + y;
+                Console.WriteLine("{0}", sum);
+                
+                Console.ForegroundColor = ConsoleColor.Red;
+                   Console.WriteLine("Do you want to run another instance? Y/N ");
+                Console.ResetColor();
+                Char exit = Console.ReadKey().KeyChar;
+                if(exit == 'Y')
+                {
+                    break;
+                }
+                Console.WriteLine(Environment.NewLine);
+                /* Console.Write("Enter wanted operator...");
+                 var sum = x + Console.ReadKey().KeyChar + y;
+                 switch (sum)
+                 {
+                     case '+' :
+                         Console.WriteLine("Result: {0}", sum);
+                         break;
+                     case '-' :
+                         Console.WriteLine("Result: {0}", sum);
+                         break;
+                     case '*':
+                         Console.WriteLine("Result: {0}", sum);
+                         break;
+                     case '/' :
+                         Console.WriteLine("Result: {0}", sum);
+                         break; 
+
+                 }  */
+            }
+            while (true);
+        }
+    }
+}
